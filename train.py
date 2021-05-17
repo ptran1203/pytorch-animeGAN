1d47f87de25eb8116fdc5f61482363a099333e16
phattran <phat.tranhoang@cyberlogitec.com>
2021-05-17 09:24:47 +0700

    log

--- train.py
+++ train.py
@@ -150,7 +150,9 @@ def main(args):
     if args.continu:
         try:
             load_checkpoint(G, args.checkpoint_dir)
+            print("G weight loaded")
             start_e = load_checkpoint(D, args.checkpoint_dir)
+            print("D weight loaded")
         except Exception as e:
             print('Could not load checkpoint, train from scratch', e)
 
