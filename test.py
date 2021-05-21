5cf5bff084f96cbaa2e8d8da31a86a9060200b9c
phattran <phat.tranhoang@cyberlogitec.com>
2021-05-21 11:25:15 +0700

    loss

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ np_img = (np_img / 127.5) - 1
 img = torch.from_numpy(np_img)
 
 yuv_img = rgb_to_yuv(img.permute(2, 0, 1).type(torch.float32).unsqueeze(0))[0]
-yuv_test = rgb_to_yuv_test(img)
+# yuv_test = rgb_to_yuv_test(img)
 
 print('yuv_img output', yuv_img.shape)
 # plt.figure()
@@ -28,15 +28,24 @@ print('yuv_img output', yuv_img.shape)
 img = img.numpy()
 
 img = np.expand_dims(img, 0)
-print(yuv_img.shape, yuv_test.shape)
+# print(yuv_img.shape, yuv_test.shape)
 
 f, ax = plt.subplots(4, 1)
-yuv_test = yuv_test.numpy()
+# yuv_test = yuv_test.numpy()
+
+kernel = np.array([
+    [0, 1, -1, 0],
+    [1, 3, -3, -1],
+    [1, 3, -3, -1],
+    [0, 1, -1, 0]
+])
+
+
+
 yuv_img = yuv_img.numpy()
 ax[0].imshow((np_img + 1) / 2)
 ax[1].imshow(yuv_img)
-ax[2].imshow(yuv_test)
-print(np.mean(yuv_img - yuv_test))
+
 plt.show()
 
 
